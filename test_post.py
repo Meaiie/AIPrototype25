3baa614cab824ea9d3dbe6293232e51843624872
Meaiie <siriyaporn.r@kkumail.com>
2025-12-25 12:21:35 +0700

    add new files

---
+++ test_post.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+url = 'http://localhost:5001/request'
+myobj = {'name':'Siriyaporn',
+         'age':21
+         }
+
+x = requests.post(url, data = json.dumps(myobj))
+output = json.loads(x.text)
+print(output['y'])
